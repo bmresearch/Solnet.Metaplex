5421ec02fca078a49d967372944778003c30c180
Mario Matić <emariomatic@gmail.com>
2021-08-17 15:55:36 +0200

    metadata CreateMetadaAccount

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-    <img src="https://raw.githubusercontent.com/mariomatic/Solnet.Metaplex/master/assets/metaplex-serum-icon.png" margin="auto" height="175"/>
+    <img src="https://raw.githubusercontent.com/mariomatic/Solnet.Metaplex/master/assets/solnet-metaplex-icon.png" margin="auto" height="100"/>
 </p>
 
 <p align="center">
--- Solnet.Metaplex/MetadataProgram.cs
+++ Solnet.Metaplex/MetadataProgram.cs
@@ -8,6 +8,8 @@ using System.Linq;
 
 namespace Solnet.Metaplex
 {
+
+
     /// <summary>
     /// Implements the Metadata program methods.
     /// <remarks>
@@ -18,14 +20,48 @@ namespace Solnet.Metaplex
     /// </summary>
     public static class MetadataProgram
     {
-            /// <summary>
-            /// The public key of the Metadata Program.
-            /// </summary>
-            public static readonly PublicKey ProgramIdKey = new("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+        /// <summary>
+        /// The public key of the Metadata Program.
+        /// </summary>
+        public static readonly PublicKey ProgramIdKey = new("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
 
-            /// <summary>
-            /// The program's name.
-            /// </summary>
-            private const string ProgramName = "Metadata Program";
+        /// <summary>
+        /// The program's name.
+        /// </summary>
+        private const string ProgramName = "Metadata Program";
+
+        /// <summary>
+        /// Create Metadata object.
+        /// </summary>
+        /// <param name="metadataKey"> Metadata key (pda of ['metadata', program id, mint id]) </param>
+        /// <param name="mintKey"> Mint of token asset </param>
+        /// <param name="authority"> Mint authority </param>
+        /// <param name="payer"> Transaction payer </param>
+        /// <param name="dataParameters"> Metadata struct with name,symbol,uri and optional list of creators </param>
+        /// <param name="isMutable"> Will the account stay mutable.</param>
+        /// <returns>The transaction instruction.</returns> 
+        public static TransactionInstruction CreateMetadataAccount (
+            PublicKey metadataKey, PublicKey mintKey, PublicKey authority, PublicKey payer, 
+            MetadataParameters dataParameters, bool isMutable
+        )
+        {
+
+            List<AccountMeta> keys = new()
+            {
+                AccountMeta.Writable(metadataKey, false),
+                AccountMeta.ReadOnly(mintKey, false),
+                AccountMeta.ReadOnly(authority, true),
+                AccountMeta.ReadOnly(payer, true),
+                AccountMeta.ReadOnly(SystemProgram.ProgramIdKey, false),
+                AccountMeta.ReadOnly(SystemProgram.SysVarRentKey, false)
+            };
+
+            return new TransactionInstruction
+            {
+                ProgramId = ProgramIdKey.KeyBytes,
+                Keys = keys,
+                Data = MetadaProgramData.EncodeCreateMetadataAccountData( dataParameters , isMutable )
+            };
+        }
     }
 }
